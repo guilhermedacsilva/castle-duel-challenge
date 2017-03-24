3c95aaff1ba7a74a8ff58e0f98e33b84b8240331
remotedeveloper88 <remotedeveloper88@gmail.com>
2017-03-24 10:29:05 -0300

    Refactored GUI

--- src/br/game/castleduel/gui/Gui.java
+++ src/br/game/castleduel/gui/Gui.java
@@ -13,95 +13,76 @@ import javax.swing.JPanel;
 import br.game.castleduel.unit.Castle;
 import br.game.castleduel.unit.Unit;
 
-public class Gui extends JFrame {
+public class Gui extends JPanel {
 	private static final long serialVersionUID = 1L;
-	protected static final int HEIGHT_UNIX = 255;
-	protected static final int HEIGHT_WINDOWS = 285;
+	protected static final int HEIGHT_GAME = 255;
+	protected BufferedImage castle = ImageLoader.load("castle.png");
+	protected BufferedImage grass = ImageLoader.load("grass.jpg");
+	protected BufferedImage sky = ImageLoader.load("sky.jpg");
+	private List<UnitSprite> deadSprites;
 
 	protected LinkedBlockingQueue<UnitSprite> sprites;
 	protected Castle castleP1;
 	protected Castle castleP2;
 	
-	public Gui() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setResizable(false);
-	}
+	public Gui() {}
 	
 	public void init(
 			int width,
 			Castle castleP1, 
 			Castle castleP2
 			) {
-		
-		final String operationalSys = System.getProperty("os.name", "linux");
-		
-		Dimension size;
-		if (operationalSys.indexOf("mac") >= 0
-				|| operationalSys.indexOf("darwin") >= 0
-				|| operationalSys.indexOf("linux") >= 0
-				|| operationalSys.indexOf("nux") >= 0
-				) {
-			size = new Dimension(width, HEIGHT_UNIX);
-		} else {
-			size = new Dimension(width, HEIGHT_WINDOWS);
-		}
-		
-		add(new DrawPanel(size));
-		pack();
-		setVisible(true);
-		setLocationRelativeTo(null);
-		
+
 		this.castleP1 = castleP1;
 		this.castleP2 = castleP2;
 		sprites = new LinkedBlockingQueue<>();
+		
+		Dimension size = new Dimension(width, HEIGHT_GAME);
+		setPreferredSize(size);
+		setSize(size);
+		deadSprites = new ArrayList<>(20);
+		
+		JFrame jframe = new JFrame("Castle Duel Challenge");
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.setResizable(false);
+		jframe.add(this);
+		jframe.pack();
+		jframe.setVisible(true);
+		jframe.setLocationRelativeTo(null);
 	}
 	
 	public void addSprite(int player, Unit unit) {
 		sprites.add(new UnitSprite(player, unit));
 	}
 
-	private class DrawPanel extends JPanel {
-		private static final long serialVersionUID = 1L;
-		protected BufferedImage castle = ImageLoader.load("castle.png");
-		protected BufferedImage grass = ImageLoader.load("grass.jpg");
-		protected BufferedImage sky = ImageLoader.load("sky.jpg");
-		private List<UnitSprite> deadSprites;
-		
-		public DrawPanel(Dimension d) {
-			setPreferredSize(d);
-			setSize(d);
-			deadSprites = new ArrayList<>(20);
-		}
-		
-		@Override
-		protected void paintComponent(Graphics g) {
-			super.paintComponent(g);
-			drawBackground(g);
-			drawUnits(g);
-		}
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		drawBackground(g);
+		drawUnits(g);
+	}
 
-		protected void drawBackground(Graphics g) {
-			g.drawImage(sky, 0, 0, null);
-			
-			for (int x = 0; x < getWidth(); x += grass.getWidth()) {
-				g.drawImage(grass, x, 165, null);
-			}
-			
-			g.drawImage(castle, 0, 0, null);
-			g.drawImage(castle, getWidth() - castle.getWidth(), 0, null);
+	protected void drawBackground(Graphics g) {
+		g.drawImage(sky, 0, 0, null);
+		
+		for (int x = 0; x < getWidth(); x += grass.getWidth()) {
+			g.drawImage(grass, x, 165, null);
 		}
 		
-		protected void drawUnits(Graphics g) {
-			deadSprites.clear();
-			for (UnitSprite sprite : sprites) {
-				sprite.paint(g);
-				if (sprite.isUnitDead()) {
-					deadSprites.add(sprite);
-				}
+		g.drawImage(castle, 0, 0, null);
+		g.drawImage(castle, getWidth() - castle.getWidth(), 0, null);
+	}
+	
+	protected void drawUnits(Graphics g) {
+		deadSprites.clear();
+		for (UnitSprite sprite : sprites) {
+			sprite.paint(g);
+			if (sprite.isUnitDead()) {
+				deadSprites.add(sprite);
 			}
-			for (UnitSprite sprite : deadSprites) {
-				sprites.remove(sprite);
-			}
+		}
+		for (UnitSprite sprite : deadSprites) {
+			sprites.remove(sprite);
 		}
 	}
 }
